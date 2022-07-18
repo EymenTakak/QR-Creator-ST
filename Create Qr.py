b4613efa3e47a4af20f8c544fcd0a867f1c0b592
EymenTakak <107555467+EymenTakak@users.noreply.github.com>
2022-07-18 04:38:48 +0300

    Add files via upload

---
+++ Create Qr.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import json
+import png
+from urllib.request import urlopen
+import pyqrcode as py
+
+st.header("Create QR Code With Your Json")
+
+js = st.text_input("Json Api", "Write Your Json Link")
+
+dictt = st.text_input("Dict Value", "Write Your Json Dict Value")
+
+z = st.number_input("Qr Scale: recommended=8", )
+
+def createqr():
+    with urlopen(js) as response:
+        kaynak = response.read()
+    global veri
+    veri = json.loads(kaynak)
+
+    t = 0
+    for res in veri:
+        t+=1
+        x = res[dictt]
+        y = py.create(x)
+        y.png(f"qr{t}.png", scale=z)
+
+if st.button("Create"):
+    createqr()
+    st.text("Wait a few minutes...")
+    time.sleep(15)
+
